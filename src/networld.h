a0a800be7ce4b234ab82bc817d155f82647be94a
Peio561 <60737155+Peio561@users.noreply.github.com>
2020-10-14 13:49:53 +0200

    Update networld.h

--- src/networld.h
+++ src/networld.h
@@ -3,6 +3,7 @@
 
 struct Str_Node {
     double x, y;
+    int mur;
 };
 typedef struct Str_Node Node;
 
@@ -11,6 +12,27 @@ struct Str_NetWorld {
     Node * nodes;
 };
 typedef struct Str_NetWorld NetWorld;
+
+//Joueur, Arme, Bonus
+struct Str_Weapon{
+    char name[100];
+    int damage;
+    Node * node;
+}typedef struct Str_Weapon Weapon;
+    
+struct Str_Player{
+    Node * node;
+    char name[100];
+    int hp=100;
+    Weapon * weapon;
+}typedef struct Str_Player Player;
+ 
+struct Str_Bonus{
+    Node * node;
+    char type[100];
+}typedef struct Str_Bonus Bonus;
+    
+    
 
 // Constructor / Destructor
 NetWorld * NetWorld_new(int aSize);
